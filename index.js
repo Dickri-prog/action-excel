6d45dbf8de22f1dd3d7364f606c11884ee5ae186
Dickri Dirgama <thedickraygomoiy58@gmail.com>
2023-08-06 19:23:08 +0700

    update index.js

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ app.get('/products/cancelled', checkingData, (req, res) => {
     console.log(dataIndexItems)
 
     dataIndexItems.forEach((item) => {
-      console.log(item, jsonDataContent[item].id, jsonDataContent[item]);
+      console.log(item, jsonDataContent[item]);
       paginatedItems.push({
         id: jsonDataContent[item].id, name: jsonDataContent[item].name
       })
